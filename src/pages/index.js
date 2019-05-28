c1d43290f1d240cd27f6ec70ea36b866a20a384a
Freal Seven <freal7@yahoo.com>
2019-05-28 14:02:37 +0100

    Clean up home page links

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,6 @@ const IndexPage = () => {
       <Head title="Home" />
       <h1>Hello</h1>
       <h2>I'm Neil, a Front-end Developer living in pollted London</h2>
-      <p>
-        Need a developer? <Link to="/contact">Contact me</Link>
-      </p>
     </Layout>
   )
 }
